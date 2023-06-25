ec886e6300a4992ea070e40781e145248d2f8654
Taneros <taneros@duck.com>
2023-06-25 10:39:22 +0300

    feat: add caterpillar orders

--- client-executor/src/components/Homepage/Homepage.tsx
+++ client-executor/src/components/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Card, Col, Row } from 'antd'
+import { Avatar, Button, Card, Col, Row, Space } from 'antd'
 import { observer } from 'mobx-react-lite'
 import { useState } from 'react'
 import appState from '../../store/appState'
@@ -6,13 +6,27 @@ import MenuMobile from './components/MenuMobile'
 import './Homepage.scss'
 
 const Homepage = (props: any) => {
-  const [accepted, setAccepted] = useState(true)
+  const [accepted, setAccepted] = useState(false)
+  const [expandedCardOrder, setExpandedCardOrder] = useState(-1)
 
   const orders = appState.getOrders()
 
   const handleAccept = () => {
     setAccepted(true)
   }
+
+  const handleExpandCard = (expandOrderId: number) => {
+    setExpandedCardOrder(expandOrderId)
+  }
+
+
+  const handleFinishOrder = (id: number) => {
+    appState.finishOrder(id).then((res) => {
+      if (res === 1) console.log(`Homepage.tsx - line: 25 ->> order ${id} delivered`)
+      else console.log(`Homepage.tsx - line: 26 ->> order not found`, )
+    })
+  }
+
 
   const ordersTotalWeight = orders.reduce((acc, val) => {
     return (
@@ -29,6 +43,16 @@ const Homepage = (props: any) => {
   const MathRound = (number: number) => {
     return (Math.round(number * 100) / 100).toFixed(2)
   }
+
+  const individualOrderCost = (order: (typeof appState.orders)[0]) =>
+    MathRound(
+      order.content.map((el) => el.cost).reduce((acc, val) => acc + val),
+    )
+
+  const individualOrderWeight = (order: (typeof appState.orders)[0]) =>
+    MathRound(
+      order.content.map((el) => el.weight).reduce((acc, val) => acc + val),
+    )
 
   return (
     <div className="homepage">
@@ -50,11 +74,15 @@ const Homepage = (props: any) => {
             <MenuMobile />
           </div>
         </header>
-  
+
         <div className="content">
           {!accepted && (
             <Card
-              style={{ width: 300, backgroundColor: '#D9D9D9', borderRadius: 8 }}
+              style={{
+                width: 300,
+                backgroundColor: '#D9D9D9',
+                borderRadius: 8,
+              }}
             >
               <p>№ 24798</p>
               <h2>Новая группа заказов</h2>
@@ -78,21 +106,91 @@ const Homepage = (props: any) => {
             <>
               {orders.map((order, idx) => {
                 return (
-                  <Card
-                    style={{
-                      width: 300,
-                      backgroundColor: '#D9D9D9',
-                      borderRadius: 8,
-                    }}
-                    key={order.id}
-                    bodyStyle={{ padding: "4px 8px 4px" }}
-                    className="homepage__card-collapsed"
-                  >
-                    <Row justify="start" align="middle">
-                      <h3 style={{margin: '0 16px 0 0'}}>{idx + 1}</h3>
-                      <h3 style={{marginBottom: 0}}>{order.addresses.start}</h3>
-                    </Row>
-                  </Card>
+                 <>
+                    <Card
+                      style={{
+                        width: 300,
+                        backgroundColor: '#D9D9D9',
+                        borderRadius: 8,
+                        marginBottom: 8,
+                      }}
+                      key={order.id}
+                      bodyStyle={{ padding: '4px 8px 4px' }}
+                      className="homepage__card-collapsed"
+                      onClick={() => handleExpandCard(order.id)}
+                    >
+                      {!(expandedCardOrder === order.id) && (
+                        <Row justify="start" align="middle">
+                          <h3 style={{ margin: '0 16px 0 0' }}>{idx + 1}</h3>
+                          <h3 style={{ marginBottom: 0 }}>
+                            {order.addresses.start}
+                          </h3>
+                        </Row>
+                      )}
+  
+                      {expandedCardOrder === order.id && (
+                        <>
+                          <Row justify="start" align="middle">
+                            <p style={{}}>Нынешний пункт</p>
+                            <p style={{ marginLeft: 16 }}>до {order.deliverTo}</p>
+                          </Row>
+                          <Row justify="space-between" align="middle">
+                            <Col span={18}>
+                              <h3>{order.addresses.start}</h3>
+                            </Col>
+                            <Col span={4}>
+                              <Avatar
+                                className="round-button"
+                                shape="circle"
+                                gap={4}
+                                size="large"
+                                style={{ backgroundColor: 'white' }}
+                              />
+                            </Col>
+                          </Row>
+                          <Row
+                            justify="start"
+                            align="middle"
+                            style={{ height: 65 }}
+                          >
+                            <p style={{}}>{order.customer.name}</p>
+                            <p style={{}}>{order.customer.phone}</p>
+                          </Row>
+  
+                          <Row justify="space-between" align="middle">
+                            <Col span={10}>
+                              <p>В заказе</p>
+                            </Col>
+                            <Col span={4}>
+                              <p>{individualOrderCost(order)} Р</p>
+                            </Col>
+                            <Col span={4}>
+                              <p>{individualOrderWeight(order)} кг</p>
+                            </Col>
+                          </Row>
+  
+                          {order.content.map((content, idx) => {
+                            return (
+                              <div key={idx + content.name}>
+                                <Row justify="space-between" align="middle">
+                                  <Col span={10}>
+                                    <p>{content.name}</p>
+                                  </Col>
+                                  <Col span={4}>
+                                    <p>{content.cost} Р</p>
+                                  </Col>
+                                  <Col span={4}>
+                                    <p>{content.weight} кг.</p>
+                                  </Col>
+                                </Row>
+                              </div>
+                            )
+                          })}
+                           <Button disabled={order.status === 1} onClick={() => handleFinishOrder(order.id)}>Завершить</Button>
+                        </>
+                      )}
+                    </Card>
+                 </>
                 )
               })}
             </>
