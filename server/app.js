639c8adcb1948b3812815fb1c8a549dadb35b241
KislerAleksandr <aka.kisler@gmail.com>
2023-06-25 14:48:36 +0300

    swagger

--- server/app.js
+++ server/app.js
@@ -26,6 +26,35 @@ app.use(bodyParser.urlencoded({extended: true, limit: '10000mb'}));
 // app.use(timeout('10000s'));
 
 app.use(router);
+const expressSwagger = require('express-swagger-generator')(app);
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'This is a sample server',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: 'localhost:8090',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routing/**/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
 
 const server = app.listen(port, '127.0.0.1', () => {
     console.log(`server working on host:${port}`);
--- server/routing/index.js
+++ server/routing/index.js
@@ -6,8 +6,30 @@ router.get("/", function (req, res) {
     res.send("hello");
 });
 
+/**
+ * This function comment is parsed by doctrine
+ * @route POST /api/auth
+ * @param {string} email.query.required - user email
+ * @returns {Array} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
 router.post("/api/auth",         auth);
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api/orders
+ * @returns {Array} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/api/orders',        getOrders);
+/**
+ * This function comment is parsed by doctrine
+ * @route POST /api/ordersStatus
+ * @param {number} id.query.required - order id
+ * @param {number} statusId.query.required  - new order Status
+ * @returns {Array} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
 router.post('/api/ordersStatus', setStatus);
 // router.post()
 module.exports = router;
